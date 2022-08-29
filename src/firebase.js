887704eeb843c58eaf2575810b89bd93e73023f1
algerr <65679099+algerr@users.noreply.github.com>
2022-08-29 19:53:39 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,6 @@ import firebase from "firebase/app";
 import 'firebase/firestore'
 import 'firebase/auth'
 
-//you can either add your firebase config directly like in the tutorial or can also add it as an 
-//json string like here https://create-react-app.dev/docs/adding-custom-environment-variables/
-
 const firebaseConfig = {
   apiKey: "AIzaSyCXYhiDT_QCSTWxKAz0LjUbzh-bXMOzafo",
   authDomain: "react-chess-5be2b.firebaseapp.com",
